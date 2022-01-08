d12a7567fd90e1e50dd444aac8ab953e77e14c18
Mike <mjpj@live.com>
2022-01-08 16:55:24 -0500

    updadted todos

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,11 +1,11 @@
 class ListController < ApplicationController
-    #Get all clinics
-    get "/list" do
-      List.all.to_json
-    end  
-    
-  end
-
-  #tinking htis will list oll the deffernt lists we he for the users ?? 
-  #what are your thuoghts
+  #Get all clinics
+  get "/list" do
+    List.all.to_json
+  end  
   
+end
+
+#tinking htis will list oll the deffernt lists we he for the users ?? 
+#what are your thuoghts
+
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,31 +1,31 @@
 class TodosController < ApplicationController
 
 
-get "/todo" do
-    todos = todo.all
-    todos.to_json({include: [user:{only: :user}]})
-   
-  end 
-
-  #Create a todo
-  post "/todo" do 
-    todo = todo.create({name:params[:name], user:params[:user], active:params[:active]})
-    todo.to_json(include: :user)
-  end 
-  #Update a todo
-
-  patch "/todo/:id" do 
-    todo = todo.find(params[:id])
-    todo.update({name:params[:name],  user:params[:user], active:params[:active]})
-    todo.to_json(include: :user)
-  end 
-  #Delete a todo
-
-  delete "/todo/:id" do
-
-    todo = todo.find(params[:id])
-    todo.destroy
-  
-    {message: 'todo deleted'}.to_json
-  end 
-end 
+    get "/todo" do
+        todos = Todo.all
+        todos.to_json
+        
+      end 
+    
+      #Create a todo
+      post "/todo" do 
+        todo = Todo.create({name:params[:name], status:params[:status]})
+        todo.to_json
+      end 
+      #Update a todo
+    
+      patch "/todo/:id" do 
+        todo = Todo.find(params[:id])
+        todo.update({name:params[:name],  status:params[:status]})
+        todo.to_json
+      end 
+      #Delete a todo
+    
+      delete "/todo/:id" do
+    
+        todo = todo.find(params[:id])
+        todo.destroy
+      
+        {message: 'todo deleted'}.to_json
+      end 
+    end 
